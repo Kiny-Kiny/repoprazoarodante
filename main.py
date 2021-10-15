935a8bcd2ecef58464d0d62e3a0217bb5b5eecb1
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-10-15 12:58:38 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ namespace Dcoder
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       Console.WriteLine("Dante Of PCW¹⁵⁷!!!");
+     }
+   }
+ }
